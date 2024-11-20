d45888b409962f6746f67be91dd57c3b34a0ef08
dabliuweb <comercial@visiontaubate.com.br>
2024-11-20 18:11:42 -0300

    fix(routes/blog/core): Redirect to /blog/ when no path is provided

--- blog/urls.py
+++ blog/urls.py
@@ -18,10 +18,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.shortcuts import redirect
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('core.urls')),
+    path('blog/', include('core.urls'), name="blog"),
+    path('', lambda request: redirect('/blog/', permanent=False))
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- core/urls.py
+++ core/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('blog/', views.blogIndex, name="blog"),
+    path('', views.blogIndex, name="blog"),
     path('post/<int:id>/', views.post, name="post"),
 ]
