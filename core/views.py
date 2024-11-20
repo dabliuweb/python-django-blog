c30fa9ab984c49997ac84ad61ff652b823025030
dabliuweb <comercial@visiontaubate.com.br>
2024-11-20 12:00:35 -0300

    feat: Create Blog template and view controller + config urls routes

--- core/views.py
+++ core/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from core.models import Post, Tag
 
 # Create your views here.
+
+def blogIndex(request):
+    template = "blog.html"
+    posts = Post.objects.all()
+    tags = Tag.objects.all()
+
+    data = {
+        'posts': posts,
+        'tags': tags
+    }
+
+    return render(request, template, data)
+
