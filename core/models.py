189168c5260b575504af4a909efcf4f3c41b3fac
dabliuweb <comercial@visiontaubate.com.br>
2024-11-20 03:44:53 -0300

    feat: Create Post and Tag Models, Register to app and make migrations

--- core/models.py
+++ core/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50)
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    tags = models.ManyToManyField(to = Tag, related_name="posts")
